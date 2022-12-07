8a372ca79ea7814e643549c1931bda0741e378f3
VISHESH  GATHA <101997238+vishesh9096@users.noreply.github.com>
2022-12-07 14:59:28 +0530

    Update Gallery.js

--- components/Gallery.js
+++ components/Gallery.js
@@ -14,7 +14,7 @@ function Gallery() {
   <div class="flex flex-col items-center gap-4 md:gap-6 sm:px-4 lg:px-8">
        <div class="flex flex-col  items-center gap-2 md:gap-3">
         <div class="w-24 md:w-28 h-32 md:h-40 bg-gray-100  overflow-hidden ">
-          <img src="/Icon.png" loading="lazy"  class="w-full  h-full object-cover object-center" />
+          <img src="./Icon.png" loading="lazy"  class="w-full  h-full object-cover object-center" />
         </div>
           <div class="text-[#21325E] text-lg  font-bold text-center sm:text-left">An Early Readers Book</div>
       </div>
@@ -23,7 +23,7 @@ function Gallery() {
   <div class="flex flex-col items-center gap-4 md:gap-6 sm:px-4 lg:px-8">
        <div class="flex flex-col  items-center gap-2 md:gap-3">
         <div class="w-28 md:w-28 h-32 md:h-40 bg-gray-100  overflow-hidden ">
-          <img src="/assignment.png" loading="lazy" alt="Photo by Radu Florin" class="w-full h-full object-cover object-center" />
+          <img src="./assignment.png" loading="lazy" alt="Photo by Radu Florin" class="w-full h-full object-cover object-center" />
         </div>
           <div class="text-[#21325E] text-lg  font-bold text-center sm:text-left">Assignments/Quizes</div>
       </div>
@@ -32,7 +32,7 @@ function Gallery() {
   <div class="flex flex-col items-center gap-4 md:gap-6 sm:px-4 lg:px-8">
        <div class="flex flex-col items-center gap-2 md:gap-3">
         <div class="w-28 md:w-36 h-32 md:h-40 bg-gray-100  overflow-hidden ">
-          <img src="/assignment.png" loading="lazy" alt="Photo by Radu Florin" class="w-full h-full object-cover object-center" />
+          <img src="./assignment.png" loading="lazy" alt="Photo by Radu Florin" class="w-full h-full object-cover object-center" />
         </div>
           <div class="text-[#21325E] text-lg  font-bold text-center sm:text-left">Reinforcement Tools</div>
       </div>
@@ -40,7 +40,7 @@ function Gallery() {
   <div class="flex flex-col items-center gap-4 md:gap-6 sm:px-4 lg:px-8">
        <div class="flex flex-col items-center gap-2 md:gap-3">
         <div class="w-28 md:w-36 h-32 md:h-40 bg-gray-100  overflow-hidden ">
-          <img src="/assignment.png" loading="lazy" alt="Photo by Radu Florin" class="w-full h-full object-cover object-center" />
+          <img src="./assignment.png" loading="lazy" alt="Photo by Radu Florin" class="w-full h-full object-cover object-center" />
         </div>
           <div class="text-[#21325E] text-lg  font-bold text-center sm:text-left">Reflection</div>
       </div>
